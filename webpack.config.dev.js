585e04dd8b471b7f7b76122abc064733630779e4
Irina <Irinafitas07@gmail.com>
2019-04-25 15:25:46 +0300

    #2_Add url-loader

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -37,7 +37,8 @@ let conf = {
                         outputPath: 'fonts/'
                     }
                 }]
-            }
+			}
+			// { test: /\.(png|woff|woff2|eot|ttf|svg)$/, loader: 'url-loader?limit=100000' }
 		]
 	},
 	plugins: [
