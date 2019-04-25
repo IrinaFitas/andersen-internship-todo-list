ba4ec5540d5c6151d694560a5ff65c3cc402b0de
Irina <Irinafitas07@gmail.com>
2019-04-25 15:04:09 +0300

    #2_Added Font

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -22,7 +22,17 @@ let conf = {
 					fallback: "style-loader",
 					use: "css-loader"
 				})
-			}
+			},
+			{
+                test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+                use: [{
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name].[ext]',
+                        outputPath: 'fonts/'
+                    }
+                }]
+            }
 		]
 	},
 	plugins: [
